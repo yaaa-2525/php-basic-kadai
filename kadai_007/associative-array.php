d1b08a3a9813556d1d1dc0cc26a64d9e243623a1
yaaa-2525 <tianxie.835@gmail.com>
2025-05-19 20:59:25 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 連想配列に値を代入する
+       $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+       print_r($personal_data);
+       ?>
+   </p>
+</body>
+
+</html>
